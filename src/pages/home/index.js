0946917c902ead660d16fbcc67243d68439efe0d
BrunoMonteiro25 <bruno.monteiro.silva25@gmail.com>
2023-04-04 22:45:08 -0300

    Logo e barra de pesquisa

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,12 +1,75 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native'
+import React, { useState } from 'react'
+import Logo from '../../components/logo'
+
+import { Ionicons } from '@expo/vector-icons'
 
 export default function Home() {
+  const [inputValue, setInputValue] = useState('')
+
+  function handleSearch() {
+    alert('você digitou: ' + inputValue)
+  }
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Logo />
+
+      <Text style={styles.title}>Encontre a receita</Text>
+      <Text style={styles.title}>que combina com você</Text>
+
+      <View style={styles.form}>
+        <TextInput
+          placeholder="Digite o nome da comida..."
+          style={styles.input}
+          value={inputValue}
+          onChangeText={(text) => setInputValue(text)}
+        />
+        <TouchableOpacity onPress={handleSearch}>
+          <Ionicons name="search" size={28} color="#4cbe6c" />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f3f9ff',
+    paddingTop: 50,
+    paddingStart: 14,
+    paddingEnd: 14,
+  },
+  title: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    color: '#0e0e0e',
+  },
+  form: {
+    backgroundColor: '#fff',
+    width: '100%',
+    borderRadius: 8,
+    marginTop: 16,
+    marginBottom: 16,
+    borderWidth: 1,
+    borderColor: '#ececec',
+    paddingLeft: 8,
+    paddingRight: 8,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  input: {
+    width: '90%',
+    maxWidth: '90%',
+    height: 54,
+  },
+})
